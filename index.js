343a2a9a24d94151eec80e40bba732e9a974a430
Mifaresss <9803211@gmail.com>
2022-09-04 20:33:27 +0300

    Task completed

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+function Population(name, species, gender, saying) {
+   this.name = name;
+   this.species = species;
+   this.gender = gender;
+   this.saying = saying;
+}
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const dog = new Population('Baxter', 'dog', 'male', 'Woof-woof!');
+const cat = new Population('Lunya', 'cat', 'female', 'Meeeow!');
+const man = new Population('Alex', 'human', 'male', 'Hello!');
+const women = new Population('Alice', 'human', 'female', 'Hi!');
 
 
+let dogPrint = '';
+for (let key in dog) {
+   dogPrint += (key + '; ' + dog[key] + '; ')
+}
+let catPrint = '';
+for (let key in cat) {
+   catPrint += (key + '; ' + cat[key] + '; ')
+}
+let manPrint = '';
+for (let key in man) {
+   manPrint += (key + '; ' + man[key] + '; ')
+}
+let womenPrint = '';
+for (let key in women) {
+   womenPrint += (key + '; ' + women[key] + '; ')
+}
+print(dogPrint);
+print(catPrint);
+print(manPrint);
+print(womenPrint);
