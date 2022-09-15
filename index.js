764a5e72659ad4bbecf0094abdee68d7336833d0
Mifaresss <9803211@gmail.com>
2022-09-15 15:26:53 +0300

    The work was redone according to the principle of OOP

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-function Population(name, species, gender, saying) {
-   this.name = name;
-   this.species = species;
-   this.gender = gender;
-   this.saying = saying;
+class Inhabitants {
+   constructor(name, species, gender, saying) {
+      this.name = name;
+      this.species = species;
+      this.gender = gender;
+      this.saying = saying;
+   }
+   prepareforPrinting() {
+      return `${this.name}; ${this.species}; ${this.gender}; ${this.saying};`
+   }
 }
 
-const dog = new Population('Baxter', 'dog', 'male', 'Woof-woof!');
-const cat = new Population('Lunya', 'cat', 'female', 'Meeeow!');
-const man = new Population('Alex', 'human', 'male', 'Hello!');
-const women = new Population('Alice', 'human', 'female', 'Hi!');
+class Humans extends Inhabitants {
+   constructor(name, species, gender, saying, legs = 2, hands = 2) {
+      super(name, species, gender, saying);
+      this.legs = legs;
+      this.hands = hands;
+   }
+   prepareforPrinting() {
+      return `${super.prepareforPrinting()} ${this.legs}; ${this.hands}`;
+   }
+}
 
-const inhabitants = [dog, cat, man, women];
-const inhabitantsProperties = ['name', 'species', 'gender', 'saying'];
+class Animals extends Inhabitants {
+   constructor(name, species, gender, saying, legs = 4) {
+      super(name, species, gender, saying);
+      this.legs = legs;
+   }
+   prepareforPrinting() {
+      return `${super.prepareforPrinting()} ${this.legs}`;
+   }
+}
 
-inhabitants.forEach(item => {
-   print(inhabitantsProperties.map(property => item[property]).join('; '))
-})
+class Dog extends Animals {
+   constructor(name, species, gender, saying, legs) {
+      super(name, species, gender, saying, legs);
+   }
+}
+class Cat extends Animals {
+   constructor(name, species, gender, saying, legs) {
+      super(name, species, gender, saying, legs);
+   }
+}
+
+const AlexMan = new Humans('Alex', 'human', 'male', 'Call me Thirteenth!');
+const JohnMan = new Humans('John', 'human', 'male', 'My name is John');
+const HarryMan = new Humans('Harry', 'human', 'male', 'Where is my magic wand?');
+
+const AliceWomen = new Humans('Alice', 'human', 'female', 'I have special DNA');
+const EmmaWomen = new Humans('Emma', 'human', 'female', 'Who is there?');
+const StacyWomen = new Humans('Stacy', 'human', 'female', 'Hi!');
+
+const BaxterDog = new Dog('Baxter', 'dog', 'male', 'Woof-woof!');
+const ArniDog = new Dog('Arni', 'dog', 'male', 'Woof-woof!');
+
+const LunyaCat = new Cat('Lunya', 'cat', 'female', 'Meeooow!');
+const TaychikCat = new Cat('Taychik', 'cat', 'male', 'Meeooow!');
+
+
+const inhabitants = [AlexMan, JohnMan, HarryMan, AliceWomen, EmmaWomen, StacyWomen, BaxterDog, ArniDog, LunyaCat, TaychikCat];
+inhabitants.forEach(inhabitant => {
+   print(inhabitant.prepareforPrinting());
+});
